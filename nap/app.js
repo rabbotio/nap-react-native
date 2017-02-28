e8835df41145ec16d9856ecb1a20f112d7944d32
katopz <katopz@gmail.com>
2017-02-28 17:40:47 +0700

    Working login and fetch from GraphQL

---
+++ nap/app.js
@@ -0,0 +1,135 @@
+const FBSDK = require('react-native-fbsdk')
+
+import React, { Component } from 'react'
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View,
+} from 'react-native'
+const {
+  LoginButton,
+  ShareDialog,
+  AccessToken,
+} = FBSDK
+
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
+
+export default class nap extends Component {
+  constructor(props) {
+    super(props)
+    const shareLinkContent = {
+      contentType: 'link',
+      contentUrl: 'https://www.facebook.com/',
+    }
+
+    this.state = {
+      shareLinkContent: shareLinkContent,
+      isLogin: false,
+      name: ''
+    }
+  }
+
+  shareLinkWithShareDialog() {
+    var tmp = this
+    ShareDialog.canShow(this.state.shareLinkContent).then(
+      function (canShow) {
+        if (canShow) {
+          return ShareDialog.show(tmp.state.shareLinkContent)
+        }
+      }
+    ).then(
+      function (result) {
+        if (result.isCancelled) {
+          alert('Share cancelled')
+        } else {
+          alert('Share success with postId: '
+            + result.postId)
+        }
+      },
+      function (error) {
+        alert('Share fail with error: ' + error)
+      }
+      )
+  }
+
+  onLoginFinished(error, result) {
+    if (error) {
+      alert("login has error: " + result.error)
+    } else if (result.isCancelled) {
+      alert("login is cancelled.")
+    } else {
+      AccessToken.getCurrentAccessToken().then(
+        (data) => {
+          // TODO : Handle bad token
+          const access_token = data.accessToken.toString()
+          console.log('access_token :', access_token)
+
+          // TODO : Secure header?
+          access_token && fetch(`http://localhost:3000/auth/facebook/token`,
+            {
+              method: 'post',
+              headers: {
+                'Authorization': `Bearer ${access_token}`
+              }
+            }
+          ).then((response) => {
+            if (response.status >= 400) {
+              throw new Error("Bad response from server")
+            }
+            return response.text()
+          }).then((text) => {
+            console.log(text)
+            this.setState({ isLogin: true })
+          })
+        }
+      )
+    }
+  }
+
+  render() {
+
+    const query = gql`{
+      userOne {
+        name
+      }
+    }`
+
+    const UserProfile = ({ data }) => this.state.isLogin ?
+      <View style={{ paddingLeft: 20, paddingTop: 20 }}>
+        <Text>Name: {data.userOne && data.userOne.name}</Text>
+      </View>
+      :
+      <View style={{ paddingLeft: 20, paddingTop: 20 }}>
+        <Text>Please log in</Text>
+      </View>
+
+    const ViewWithData = graphql(query, {
+      options: { variables: { name: this.state.name } }
+    })(UserProfile)
+
+    return (
+      <View style={styles.container}>
+        <LoginButton
+          onLoginFinished={this.onLoginFinished.bind(this)}
+          onLogoutFinished={() => alert("logout.")} />
+        <ViewWithData />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  shareText: {
+    fontSize: 20,
+    margin: 10,
+  },
+})
--- nap/index.ios.js
+++ nap/index.ios.js
@@ -1,120 +1,21 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React from 'react'
+import { AppRegistry } from 'react-native'
+import App from './app'
 
-const FBSDK = require('react-native-fbsdk');
+import ApolloClient, { createNetworkInterface } from 'apollo-client'
+import { ApolloProvider } from 'react-apollo'
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  TouchableHighlight,
-  View,
-} from 'react-native';
-const {
-  LoginButton,
-  ShareDialog,
-  AccessToken,
-} = FBSDK;
-
-export default class nap extends Component {
-  constructor(props) {
-    super(props);
-    const shareLinkContent = {
-      contentType: 'link',
-      contentUrl: 'https://www.facebook.com/',
-    };
-
-    this.state = {
-      shareLinkContent: shareLinkContent,
-    };
-  }
-
-  shareLinkWithShareDialog() {
-    var tmp = this;
-    ShareDialog.canShow(this.state.shareLinkContent).then(
-      function (canShow) {
-        if (canShow) {
-          return ShareDialog.show(tmp.state.shareLinkContent);
-        }
-      }
-    ).then(
-      function (result) {
-        if (result.isCancelled) {
-          alert('Share cancelled');
-        } else {
-          alert('Share success with postId: '
-            + result.postId);
-        }
-      },
-      function (error) {
-        alert('Share fail with error: ' + error);
-      }
-      );
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <LoginButton
-          onLoginFinished={
-            (error, result) => {
-              if (error) {
-                alert("login has error: " + result.error);
-              } else if (result.isCancelled) {
-                alert("login is cancelled.");
-              } else {
-                AccessToken.getCurrentAccessToken().then(
-                  (data) => {
-                    // TODO : Handle bad token
-                    const access_token = data.accessToken.toString()
-                    console.log('access_token :', access_token)
-
-                    // TODO : Secure header?
-                    access_token && fetch(`http://localhost:3000/auth/facebook/token`,
-                      {
-                        method: 'post',
-                        headers: {
-                          'Authorization': `Bearer ${access_token}`
-                        }
-                      }
-                    ).then((response) => {
-                      if (response.status >= 400) {
-                        throw new Error("Bad response from server");
-                      }
-                      return response.text();
-                    }).then((text) => {
-                      console.log(text);
-                    })
-                  }
-                )
-              }
-            }
-          }
-          onLogoutFinished={() => alert("logout.")} />
-        <TouchableHighlight style={styles.share}
-          onPress={this.shareLinkWithShareDialog.bind(this)}>
-          <Text style={styles.shareText}>Share link with ShareDialog</Text>
-        </TouchableHighlight>
-      </View>
-    );
-  }
+const nap = () => {
+  const networkInterface = createNetworkInterface({
+    uri: 'http://localhost:3000/graphql'
+  })
+  const client = new ApolloClient({
+    networkInterface
+  })
+  return (
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>)
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  shareText: {
-    fontSize: 20,
-    margin: 10,
-  },
-});
-
-AppRegistry.registerComponent('nap', () => nap);
+AppRegistry.registerComponent('nap', () => nap)
