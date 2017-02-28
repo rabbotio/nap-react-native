28089fdbe10e871a70c6d9d53b473bcb589c5aa0
katopz <katopz@gmail.com>
2017-02-28 15:21:53 +0700

    Working fb sdk

--- nap/index.android.js
+++ nap/index.android.js
@@ -1,31 +1,82 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
+ * Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+ *
+ * You are hereby granted a non-exclusive, worldwide, royalty-free license to use,
+ * copy, modify, and distribute this software in source code or binary form for use
+ * in connection with the web services and APIs provided by Facebook.
+ *
+ * As with any software that integrates with the Facebook platform, your use of
+ * this software is subject to the Facebook Developer Principles and Policies
+ * [http://developers.facebook.com/policy/]. This copyright notice shall be
+ * included in all copies or substantial portions of the software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+'use strict';
+
+const FBSDK = require('react-native-fbsdk');
 
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  TouchableHighlight,
+  View,
 } from 'react-native';
+const {
+  LoginButton,
+  ShareDialog,
+} = FBSDK;
 
-export default class nap extends Component {
+class nap extends Component {
+  constructor(props) {
+    super(props);
+    const shareLinkContent = {
+      contentType: 'link',
+      contentUrl: 'https://www.facebook.com/',
+    };
+
+    this.state = {
+      shareLinkContent: shareLinkContent,
+    };
+  }
+
+  shareLinkWithShareDialog() {
+    var tmp = this;
+    ShareDialog.canShow(this.state.shareLinkContent).then(
+      function(canShow) {
+        if (canShow) {
+          return ShareDialog.show(tmp.state.shareLinkContent);
+        }
+      }
+    ).then(
+      function(result) {
+        if (result.isCancelled) {
+          alert('Share cancelled');
+        } else {
+          alert('Share success');
+        }
+      },
+      function(error) {
+        alert('Share fail with error: ' + error);
+      }
+    );
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+        <LoginButton />
+        <TouchableHighlight style={styles.share}
+          onPress={this.shareLinkWithShareDialog.bind(this)}>
+          <Text style={styles.shareText}>Share link with ShareDialog</Text>
+        </TouchableHighlight>
       </View>
     );
   }
@@ -38,15 +89,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
+  shareText: {
     fontSize: 20,
-    textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
 
--- nap/index.ios.js
+++ nap/index.ios.js
@@ -4,28 +4,101 @@
  * @flow
  */
 
+const FBSDK = require('react-native-fbsdk');
+
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  TouchableHighlight,
+  View,
 } from 'react-native';
+const {
+  LoginButton,
+  ShareDialog,
+  AccessToken,
+} = FBSDK;
 
 export default class nap extends Component {
+  constructor(props) {
+    super(props);
+    const shareLinkContent = {
+      contentType: 'link',
+      contentUrl: 'https://www.facebook.com/',
+    };
+
+    this.state = {
+      shareLinkContent: shareLinkContent,
+    };
+  }
+
+  shareLinkWithShareDialog() {
+    var tmp = this;
+    ShareDialog.canShow(this.state.shareLinkContent).then(
+      function (canShow) {
+        if (canShow) {
+          return ShareDialog.show(tmp.state.shareLinkContent);
+        }
+      }
+    ).then(
+      function (result) {
+        if (result.isCancelled) {
+          alert('Share cancelled');
+        } else {
+          alert('Share success with postId: '
+            + result.postId);
+        }
+      },
+      function (error) {
+        alert('Share fail with error: ' + error);
+      }
+      );
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <LoginButton
+          onLoginFinished={
+            (error, result) => {
+              if (error) {
+                alert("login has error: " + result.error);
+              } else if (result.isCancelled) {
+                alert("login is cancelled.");
+              } else {
+                AccessToken.getCurrentAccessToken().then(
+                  (data) => {
+                    // TODO : Handle bad token
+                    const access_token = data.accessToken.toString()
+                    console.log('access_token :', access_token)
+
+                    // TODO : Secure header?
+                    access_token && fetch(`http://localhost:3000/auth/facebook/token`,
+                      {
+                        method: 'post',
+                        headers: {
+                          'Authorization': `Bearer ${access_token}`
+                        }
+                      }
+                    ).then((response) => {
+                      if (response.status >= 400) {
+                        throw new Error("Bad response from server");
+                      }
+                      return response.text();
+                    }).then((text) => {
+                      console.log(text);
+                    })
+                  }
+                )
+              }
+            }
+          }
+          onLogoutFinished={() => alert("logout.")} />
+        <TouchableHighlight style={styles.share}
+          onPress={this.shareLinkWithShareDialog.bind(this)}>
+          <Text style={styles.shareText}>Share link with ShareDialog</Text>
+        </TouchableHighlight>
       </View>
     );
   }
@@ -38,15 +111,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
+  shareText: {
     fontSize: 20,
-    textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
 
